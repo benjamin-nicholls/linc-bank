b8dc0bf88a7b1b2c399056dabf2a6fe66c28d66e
Benjamin Horner <benjaminpnicholls@gmail.com>
2022-02-07 13:43:01 +0000

    Added deposit and withdraw methods.

--- savings.cpp
+++ savings.cpp
@@ -1,5 +1,32 @@
+#include "savings.h"
+
+savings::savings() {
+
+}
+
+savings::~savings() {
+
+}
+
+bool savings::deposit(double amount) {
+	if (amount <= 0) { return false; }
+	balance += amount;
+	historyinfo* hi = new historyinfo("Deposit", amount);
+	history.push_back(hi);
+	return true;
+}
 
 
+bool savings::withdraw(double amount) {
+	if (amount <= 0) { return false; }
+	if (balance - amount < 0) { return false; }
+
+	balance -= amount;
+
+	historyinfo* hi = new historyinfo("Withdraw", amount);
+	history.push_back(hi);
+	return true;
+}
 
 /*
 
