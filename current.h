f48d92048b437e0e92e371a92319e9892311d2a6
Ben <benjaminpnicholls@gmail.com>
2022-06-13 14:29:06 +0200

    Modifying/adding methods. / ToString made public. + GetBalance() added. + Truncate2dp() added.

--- current.h
+++ current.h
@@ -8,12 +8,14 @@ public:
 	Current();
 	Current(double InitialDeposit);
 	~Current();
+	std::string ToString();
 
 protected:
 	double m_OverdraftLimit = 500.00;
 	double m_Overdraft;
 	bool Deposit(double Amount);
 	bool Withdraw(double Amount);
-	std::string ToString();
+	double GetBalance();
+	std::string Truncate2dp(double Value);
 
 };
