7e65bfca94a87c5489fc767311cd9851d3d99864
Ben <benjaminpnicholls@gmail.com>
2022-06-13 14:30:51 +0200

    + Added ToString(). / Formatted 0 as 0.00 to show currency. + Added GetBalance(). + Added Truncate2dp().

--- savings.h
+++ savings.h
@@ -6,8 +6,9 @@
 class Savings: public Account, public InterestEarning {
 
 public:
-	Savings(double InitialDeposit = 0, bool Isa = false);
+	Savings(double InitialDeposit = 0.00, bool Isa = false);
 	~Savings();
+	std::string ToString();
 
 protected:
 	double m_InterestRate = 0.85;
@@ -16,5 +17,7 @@ protected:
 	bool Withdraw(double Amount);
 	void AssignInterest();
 	double ComputeInterest(double InitialBalance, int Years);
+	double GetBalance();
+	std::string Truncate2dp(double Value);
 	
 };
