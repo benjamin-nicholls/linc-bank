884ff73f976e4e9edad8f9446808c8086cb79ace
Ben <benjaminpnicholls@gmail.com>
2022-06-13 14:30:08 +0200

    + Added chrono. + Added more member variables. + Added ToString(). - Removed GetType(). - Removed GetValue().

--- historyinfo.h
+++ historyinfo.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <string>
-
+#include <chrono>
 
 class HistoryInfo {
 private:
+	std::string m_Currency;
 	std::string m_Type;
 	double m_Value;
+	std::time_t m_Time;
+	std::string Truncate2dp(double Value);
 
 public:
-	HistoryInfo();
 	HistoryInfo(std::string Type, double Value);
 	~HistoryInfo();
-	std::string GetType();
-	double GetValue();
+	std::string ToString();
+
 
 };
