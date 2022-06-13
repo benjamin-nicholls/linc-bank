abe6deb8f1dcc54d0c9b80d857e72f296d3a9eec
Ben <benjaminpnicholls@gmail.com>
2022-06-13 16:11:46 +0200

    / Formatting changes. - Removing commented exceptions. + Implementing ComputeInterest method.

--- savings.cpp
+++ savings.cpp
@@ -4,29 +4,26 @@
 
 Savings::Savings(double InitialDeposit, bool Isa) {
 	if (InitialDeposit < 0) {
-		//throw std::exception{"Initial deposit for a savings account cannot be negative."};
 		throw "Initial deposit for a savings account cannot be negative.";
 	}
-
 	m_Balance = InitialDeposit;
 	m_ISA = Isa;
 	std::string accountType = "Savings";
 	if (m_ISA) {
 		if (InitialDeposit < 1000) {
-			//throw std::exception& e("Initial deposit for an ISA account must be at least �1000.");
 			throw "Initial deposit for an ISA account must be at least £1000.";
 		}
 		m_InterestRate = 1.15;
 		accountType = "ISA";
 	}
-
 	Transaction* t = new Transaction("Open " + accountType + " Account", m_Balance);
 	m_History.push_back(t);
 }	
 
-Savings::~Savings() {
 
+Savings::~Savings() {
 }
+
 
 bool Savings::Deposit(double Amount) {
 	if (Amount <= 0) { return false; }
@@ -40,21 +37,19 @@ bool Savings::Deposit(double Amount) {
 bool Savings::Withdraw(double Amount) {
 	if (Amount <= 0.00) { return false; }
 	if (m_Balance - Amount < 0.00) { return false; }
-
 	m_Balance -= Amount;
-
 	Transaction* t = new Transaction("Withdraw", Amount);
 	m_History.push_back(t);
 	return true;
 }
 
 
-double Savings::ComputeInterest(double InitialBalance, int Years) {
-	// Compound interest equation.
-	// A = P(1+(r/n))^nt
-	// A = final amount, P = intial balance, r = interest rate decimal, t = time period, n = applied per unit time (monthly).
-	return InitialBalance * pow(1 + m_InterestRate / 12, 12 * Years);
+double Savings::ComputeInterest(int Years) {
+	int n = 12;  // Unit time (monthly = 12).
+	double finalAmount = m_Balance * pow(1 + m_InterestRate / n, n * Years);
+	return finalAmount;
 }
+
 
 std::string Savings::Truncate2dp(double Value) {
 	std::string value = std::to_string(Value);
@@ -65,7 +60,6 @@ std::string Savings::Truncate2dp(double Value) {
 
 std::string Savings::ToString() {
 	std::string a = "";
-
 	a += "Savings account | Balance: £" + Savings::Truncate2dp(m_Balance) + "\n";
 	for (auto entry : m_History) {
 		a += entry->ToString();
