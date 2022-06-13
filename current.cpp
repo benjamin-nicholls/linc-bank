bba879e08219b3c81bc4e08b13d15880af74f012
Ben <benjaminpnicholls@gmail.com>
2022-06-13 14:34:07 +0200

    / Changed 0 to 0.00 as currency. + Added GetBalance() method. / Implemented ToString() method.

--- current.cpp
+++ current.cpp
@@ -2,8 +2,8 @@
 
 
 Current::Current() {
-	m_Balance = 0;
-	m_Overdraft = 0;
+	m_Balance = 0.00;
+	m_Overdraft = 0.00;
 	HistoryInfo* hi = new HistoryInfo("Open Current Account", m_Balance);
 	m_History.push_back(hi);
 	//delete hi; //will this delete the data or just the pointer?
@@ -11,13 +11,13 @@ Current::Current() {
 
 
 Current::Current(double InitialDeposit) {
-	if (InitialDeposit < 0) {
+	if (InitialDeposit < 0.00) {
 		//throw std::exception{ "Initial deposit for a current account cannot be negative." };
 		throw "Initial deposit for a current account cannot be negative.";
 	}
 	m_Balance = InitialDeposit;
-	m_Overdraft = 0;
-	HistoryInfo* hi = new HistoryInfo("Open Current Account", m_Balance);
+	m_Overdraft = 0.00;
+	HistoryInfo* hi = new HistoryInfo("Open Current Acount", m_Balance);
 	m_History.push_back(hi);
 }
 
@@ -27,11 +27,11 @@ Current::~Current() {
 
 
 bool Current::Deposit(double Amount) {
-	if (Amount <= 0) { return false; }
+	if (Amount <= 0.00) { return false; }
 
 	double a = Amount - m_Overdraft;
 	if (a > 0) {
-		m_Overdraft = 0;
+		m_Overdraft = 0.00;
 		m_Balance += a;
 	} else {
 		m_Overdraft = -a;
@@ -45,13 +45,13 @@ bool Current::Deposit(double Amount) {
 
 
 bool Current::Withdraw(double Amount) {
-	if (Amount <= 0) { return false; }
+	if (Amount <= 0.00) { return false; }
 	if (m_Balance + m_OverdraftLimit - m_Overdraft <= Amount) { return false; }
 
 	m_Balance -= Amount;
-	if (m_Balance < 0) {
+	if (m_Balance < 0.00) {
 		m_Overdraft = -m_Balance;
-		m_Balance = 0;
+		m_Balance = 0.00;
 	}
 
 	HistoryInfo* hi = new HistoryInfo("Withdraw", Amount);
@@ -60,6 +60,25 @@ bool Current::Withdraw(double Amount) {
 }
 
 
-std::string Current::ToString() {
-	return "";
+double Current::GetBalance() {
+	return m_Balance - m_Overdraft;
 }
+
+
+std::string Current::Truncate2dp(double Value) {
+	std::string value = std::to_string(Value);
+	value = value.substr(0, value.find('.')  + 3);
+	return value;
+}
+
+
+std::string Current::ToString() {
+	std::string a = "";
+
+	a += "Current account | Balance: £" + Current::Truncate2dp(this->GetBalance()) + "\n";
+	for (auto entry : m_History) {
+		a += entry->ToString() + "\n";
+    }
+	return a;                                                           
+}
+
