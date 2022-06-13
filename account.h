a6fea8bcf9454962d0862eba975de058c468951d
Ben <benjaminpnicholls@gmail.com>
2022-06-13 14:28:10 +0200

    ToString made pure virtual.

--- account.h
+++ account.h
@@ -9,14 +9,13 @@ class Account {
 public:
 	Account() {}
 	~Account() {}
+	virtual std::string ToString() = 0;
 
 protected:
 	double m_Balance;
 	std::vector<HistoryInfo*> m_History;
 	double m_OverdraftLimit = 0;
-
 	virtual bool Deposit(double Amount) = 0;
 	virtual bool Withdraw(double Amount) = 0;
-	std::string ToString();
 
 };
