8571393f98d693c0e73bbd9489108e050061d21f
Ben <benjaminpnicholls@gmail.com>
2022-06-13 14:56:08 +0200

    Made Deposit() and Withdraw() public.

--- account.h
+++ account.h
@@ -5,17 +5,15 @@
 
 
 class Account {
-
 public:
 	Account() {}
 	~Account() {}
+	virtual bool Deposit(double Amount) = 0;
+	virtual bool Withdraw(double Amount) = 0;
 	virtual std::string ToString() = 0;
 
 protected:
 	double m_Balance;
 	std::vector<Transaction*> m_History;
 	double m_OverdraftLimit = 0;
-	virtual bool Deposit(double Amount) = 0;
-	virtual bool Withdraw(double Amount) = 0;
-
 };
--- savings.h
+++ savings.h
@@ -4,20 +4,18 @@
 
 
 class Savings: public Account, public InterestEarning {
-
 public:
 	Savings(double InitialDeposit = 0.00, bool Isa = false);
 	~Savings();
+	bool Deposit(double Amount);
+	bool Withdraw(double Amount);
 	std::string ToString();
 
 protected:
 	double m_InterestRate = 0.85;
 	bool m_ISA;
-	bool Deposit(double Amount);
-	bool Withdraw(double Amount);
 	void AssignInterest();
 	double ComputeInterest(double InitialBalance, int Years);
 	double GetBalance();
 	std::string Truncate2dp(double Value);
-	
 };
