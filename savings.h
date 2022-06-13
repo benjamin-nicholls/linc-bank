780fddc015cee3019f6ba51fc5fc04f8d65bdd49
Ben <benjaminpnicholls@gmail.com>
2022-06-13 16:10:31 +0200

    - Removing AssignInterest(). / Making ComputeInterest() public.

--- savings.h
+++ savings.h
@@ -9,13 +9,12 @@ public:
 	~Savings();
 	bool Deposit(double Amount);
 	bool Withdraw(double Amount);
+	double ComputeInterest(int Years);
 	std::string ToString();
 
 protected:
 	double m_InterestRate = 0.85;
 	bool m_ISA;
-	void AssignInterest();
-	double ComputeInterest(double InitialBalance, int Years);
 	double GetBalance();
 	std::string Truncate2dp(double Value);
 };
