e691c763c06da6e0675d2a2a0f16cabf0699e974
Ben <benjaminpnicholls@gmail.com>
2022-06-14 20:54:56 +0200

    Lots of custom exceptions. Adding in int Ref to Deposit() and Withdraw().

--- account.h
+++ account.h
@@ -8,8 +8,8 @@ class Account {
 public:
 	Account() {}
 	~Account() {}
-	virtual bool Deposit(double Amount) = 0;
-	virtual bool Withdraw(double Amount) = 0;
+	virtual bool Deposit(double Amount, int Ref = 0) = 0;
+	virtual bool Withdraw(double Amount, int Ref = 0) = 0;
 	virtual std::string ToString() const = 0;
 
 protected:
@@ -17,3 +17,85 @@ protected:
 	std::vector<Transaction*> m_History;
 	double m_OverdraftLimit = 0;
 };
+
+
+// Custom exceptions.
+struct InitialDepositBelowZeroException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: Initial deposit must be above £0.00.";
+	}
+};
+
+
+struct InitialDepositBelowISARequiredException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: Initial deposit must be at least £1000.";
+	}
+};
+
+
+struct DynamicCastUnsuccessfulException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: Selected account is not a savings account.";
+	}
+};
+
+
+struct NotIntException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: Value was not an integer.";
+	}
+};
+
+
+struct NotCurrencyException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: Value was not a currency format.";
+	}
+};
+
+
+struct NoAccountsCreatedException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: You have not opened any accounts yet.";
+	}
+};
+
+
+struct NotEnoughParametersException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: You do not have enough parameters.";
+	}
+};
+
+
+struct AccountNumberOutOfRangeException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: Account number specified out of range.";
+	}
+};
+
+
+struct MaxCurrentAccountException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: You have opened the maximum number of current account(s).";
+	}
+};
+
+
+struct MaxIsaAccountException : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "ERROR: You have opened the maximum number of ISA saving account(s).";
+	}
+};
+
