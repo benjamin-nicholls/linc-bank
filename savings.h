19a9e611beb17ad06192e484f698a01ab05aa168
Ben <benjaminpnicholls@gmail.com>
2022-06-14 20:57:02 +0200

    Deposit and Withdaw now have int Ref for transfers. m_InterestRate is no longer initialised in .h.

--- savings.h
+++ savings.h
@@ -7,13 +7,13 @@ class Savings: public Account, public InterestEarning {
 public:
 	Savings(double InitialDeposit = 0.00, bool Isa = false);
 	~Savings();
-	bool Deposit(double Amount);
-	bool Withdraw(double Amount);
+	bool Deposit(double Amount, int Ref = 0);
+	bool Withdraw(double Amount, int Ref = 0);
 	double ComputeInterest(int Years) const;
 	std::string ToString() const;
 
 protected:
-	double m_InterestRate = 0.85;
+	double m_InterestRate;
 	bool m_ISA;
 	std::string Truncate2dp(double Value) const;
 };
