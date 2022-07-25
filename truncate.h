afae82e547b213af94a3950c9bd593a8fb40a40b
Ben <benjaminpnicholls@gmail.com>
2022-07-25 13:27:54 +0200

    Add comment.

--- truncate.h
+++ truncate.h
@@ -4,6 +4,7 @@
 
 class Truncate {
 public:
+	// Inline method to reduce the number of files.
 	static std::string Truncate2dp(double Value) {
 		std::string value = std::to_string(Value);
 		return value.substr(0, value.find('.')  + 3);
