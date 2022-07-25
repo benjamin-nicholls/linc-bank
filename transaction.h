4d412d1b1265f92b73b19177b26faa2ed8482ed9
Ben <benjaminpnicholls@gmail.com>
2022-07-25 13:28:39 +0200

    Removing <chrono>. Reorder to be public and then private.

--- transaction.h
+++ transaction.h
@@ -1,23 +1,23 @@
 #pragma once
 #include <string>
-#include <chrono>
 
 
 class Transaction {
-private:
-	std::string m_Currency;
-	std::string m_Type;
-	double m_Value;
-	std::time_t m_Time;
-
 public:
 	Transaction(std::string Type, double Value);
 	~Transaction() {};
 	std::string toString() const;
+
 	bool operator <(Transaction const &obj);
 	bool operator >(Transaction const &obj);
 	bool operator ==(Transaction const &obj);
 	bool operator <(double const Key);
 	bool operator >(double const Key);
 	bool operator ==(double const Key);
+
+private:
+	std::string m_Currency;
+	std::string m_Type;
+	double m_Value;
+	std::time_t m_Time;
 };
