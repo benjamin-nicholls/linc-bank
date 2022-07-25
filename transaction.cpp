6ce66d00ce0f4ed8bdef069d1cbe3ab0ef12e9a1
Ben <benjaminpnicholls@gmail.com>
2022-07-25 15:44:53 +0200

    Changes for running on Windows.

--- transaction.cpp
+++ transaction.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "transaction.h"
 #include "truncate.h"
 #include <chrono>  // Time of transaction.
--- transaction.h
+++ transaction.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
-
+#include <ctime>
 
 class Transaction {
 public:
