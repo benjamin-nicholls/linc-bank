e0368c701b95405fbb233607e71ce9c96ac15e2a
Ben <benjaminpnicholls@gmail.com>
2022-07-25 13:29:44 +0200

    Remove unneeded includes.

--- bst.cpp
+++ bst.cpp
@@ -1,6 +1,4 @@
 #include "bst.h"
-#include <cmath>
-#include <iostream>  // remove
 
 
 TreeNode::TreeNode() {
@@ -69,7 +67,6 @@ bool TreeNode::operator ==(double const Key) {
 
 
 // BST
-
 TreeNode* BST::Search(TreeNode* &p_Node, double Key) {
     if (p_Node == nullptr) { return nullptr; }
     if (*p_Node == Key) { return p_Node; }  // Need to check for ALL instances of this value.
@@ -80,77 +77,46 @@ TreeNode* BST::Search(TreeNode* &p_Node, double Key) {
 
 
 TreeNode* BST::Insert(TreeNode* &p_Node, Transaction* p_Transaction) {
-    BST::InsertTransaction(p_Node, p_Transaction);
-    int heightDifference = abs(BST::Height(p_Node->m_p_Left) - BST::Height(p_Node->m_p_Right));
-    std::cout << "insert" << "\n";
-    BST::FixImbalance(p_Node);
-    //if (heightDifference > 3) { p_Node = BST::Rebalance(p_Node); }
+    BST::insertTransaction(p_Node, p_Transaction);
+    BST::rebalance(p_Node);
     return p_Node;
 }
 
-TreeNode* BST::InsertTransaction(TreeNode* &p_Node, Transaction* p_Transaction) {
+
+TreeNode* BST::insertTransaction(TreeNode* &p_Node, Transaction* p_Transaction) {
     if (p_Node == nullptr) { p_Node = new TreeNode(p_Transaction); return p_Node; }
     if (*p_Node == *p_Transaction) { p_Node->m_TransactionOverflow.push_back(p_Transaction); }
-    if (*p_Node > *p_Transaction) { BST::InsertTransaction(p_Node->m_p_Left, p_Transaction); }
-    if (*p_Node < *p_Transaction) { BST::InsertTransaction(p_Node->m_p_Right, p_Transaction); }
+    if (*p_Node > *p_Transaction) { BST::insertTransaction(p_Node->m_p_Left, p_Transaction); }
+    if (*p_Node < *p_Transaction) { BST::insertTransaction(p_Node->m_p_Right, p_Transaction); }
     return p_Node;
 }
 
 
-TreeNode* BST::InsertNode(TreeNode* &p_Node, TreeNode* p_NewNode) {
-    // Adding a whole node during insert is only used during rebalancing.
-    // No need to check for == as this will never occur.
-    if (p_Node == nullptr) { p_Node = p_NewNode; return p_Node; }
-    if (*p_Node > *p_NewNode) { BST::InsertNode(p_Node->m_p_Left, p_NewNode); }
-    if (*p_Node < *p_NewNode) { BST::InsertNode(p_Node->m_p_Right, p_NewNode); }
-    return p_Node;
-}
-
-
-int BST::Height(TreeNode* &p_Node) {
-    // If you use root node, you get maximum height of the tree.
-    // If you use left or right of root node, you can check for an imbalance.
+int BST::height(TreeNode* &p_Node) {
     if (p_Node == nullptr) { return 0; }
-    int leftHeight = BST::Height(p_Node->m_p_Left);
-    int rightHeight = BST::Height(p_Node->m_p_Right);
+    int leftHeight = BST::height(p_Node->m_p_Left);
+    int rightHeight = BST::height(p_Node->m_p_Right);
     if (leftHeight > rightHeight) { return leftHeight + 1; };
     return rightHeight + 1;
 }
 
 
-TreeNode* BST::FixImbalance(TreeNode* &p_Node) {
+TreeNode* BST::rebalance(TreeNode* &p_Node) {
     if (p_Node == nullptr) { return p_Node; }
-    int leftHeight = BST::Height(p_Node->m_p_Left);
-    int rightHeight = BST::Height(p_Node->m_p_Right);
-    std::cout << "root is: " << p_Node->toString() << "\n";
-    std::cout << "height: " << BST::Height(p_Node) << "\n";
-    std::cout << "height L: " << BST::Height(p_Node->m_p_Left) << "\n";
-    std::cout << "height R: " << BST::Height(p_Node->m_p_Right) << "\n";
-
-    // If imbalance, fix.
-    if (abs(leftHeight - rightHeight) > 1) {
-        if (leftHeight > rightHeight) {
-            std::cout << "ROTATING RIGHT" << "\n";
-            p_Node = BST::RotateRight(p_Node);
-        } else {
-            std::cout << "ROTATING LEFT" << "\n";
-            p_Node = BST::RotateLeft(p_Node);
-
-        }
-    } else {
-        std::cout << "NO ROTATION" << "\n";
+    int leftHeight = BST::height(p_Node->m_p_Left);
+    int rightHeight = BST::height(p_Node->m_p_Right);
+    if (leftHeight - rightHeight > 1) { 
+        p_Node = BST::rotateRight(p_Node);
+    } else if (leftHeight - rightHeight < -1) {
+        p_Node = BST::rotateLeft(p_Node);
     }
-    std::cout << "height: " << BST::Height(p_Node) << "\n";
-    std::cout << "height L: " << BST::Height(p_Node->m_p_Left) << "\n";
-    std::cout << "height R: " << BST::Height(p_Node->m_p_Right) << "\n";
-
-    FixImbalance(p_Node->m_p_Left);
-    FixImbalance(p_Node->m_p_Right);
+    rebalance(p_Node->m_p_Left);
+    rebalance(p_Node->m_p_Right);
     return p_Node;
 }
 
 
-TreeNode* BST::RotateRight(TreeNode* &p_Node) {
+TreeNode* BST::rotateRight(TreeNode* &p_Node) {
     TreeNode* p_A = p_Node;
     TreeNode* p_B = p_Node->m_p_Left;
     p_A->m_p_Left = p_B->m_p_Right;
@@ -159,73 +125,10 @@ TreeNode* BST::RotateRight(TreeNode* &p_Node) {
 }
 
 
-TreeNode* BST::RotateLeft(TreeNode* &p_Node) {
+TreeNode* BST::rotateLeft(TreeNode* &p_Node) {
     TreeNode* p_A = p_Node;
     TreeNode* p_B = p_Node->m_p_Right;
     p_A->m_p_Right = p_B->m_p_Left;
     p_B->m_p_Left = p_A;
     return p_B;
 }
-
-
-
-
-
-TreeNode* BST::Rebalance(TreeNode* &p_Node) {
-    std::vector<TreeNode*> *nodeList = new std::vector<TreeNode*>;
-    BST::ListNodesInOrder(p_Node, nodeList);
-    int size = nodeList->size();
-    auto mid = nodeList->begin() + size / 2;
-    TreeNode* newRoot;
-    BST::RebalanceInsert(newRoot, nodeList);
-    nodeList->clear();
-    return newRoot;
-}
-
-
-void BST::RebalanceInsert(TreeNode* &p_Node, std::vector<TreeNode*> *nodeList) {
-    auto mid = nodeList->begin() + nodeList->size() / 2;
-    // Find specific index, not memory address.
-    int midIndex = 0;
-    for (int i = 0; i < nodeList->size(); i++) {
-        if (nodeList->at(i) == *mid) {
-            midIndex = i;
-            break;
-        }
-    }
-    BST::InsertNode(p_Node, *mid);
-    TreeNode* temp = *mid;
-    if (nodeList->size() == 1) { return; }
-    std::vector<TreeNode*> *leftNodes = new std::vector<TreeNode*>;
-    for (auto it = 0; it != midIndex; it++) { leftNodes->push_back(nodeList->at(it)); }
-    if (!leftNodes->empty()) { RebalanceInsert(p_Node, leftNodes); }
-    std::vector<TreeNode*> *rightNodes = new std::vector<TreeNode*>;
-    for (auto it = midIndex; it != nodeList->size(); it++) { rightNodes->push_back(nodeList->at(it)); }
-    if (!rightNodes->empty()) { RebalanceInsert(p_Node, rightNodes); }
-    leftNodes->clear();
-    rightNodes->clear();
-    return;
-}
-
-void BST::ListNodesInOrder(TreeNode* &p_Node, std::vector<TreeNode*> *nodeList) {
-    // nodeList pointer is used to update vector.
-    if (p_Node->m_p_Left == nullptr) {
-        nodeList->push_back(p_Node);
-    } else {
-        ListNodesInOrder(p_Node->m_p_Left, nodeList);
-        nodeList->push_back(p_Node);
-    }
-    if (p_Node->m_p_Right == nullptr) { return; }
-    ListNodesInOrder(p_Node->m_p_Right, nodeList);
-    return;
-}
-
-
-// Not used functionally within the bank program.
-void BST::PrintTreeInOrder(TreeNode* p_Node) {
-    if (p_Node == nullptr) { return; }
-    PrintTreeInOrder(p_Node->m_p_Left);
-    std::cout << p_Node->m_p_Transaction->toString();  // No endl needed.
-    PrintTreeInOrder(p_Node->m_p_Right);
-}
-
