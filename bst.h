307b01b4f02981d1bc16d7f94e047d32abaed3ed
Ben <benjaminpnicholls@gmail.com>
2022-07-25 13:30:07 +0200

    Remove unneeded/unused methods. Remove <cmath>.

--- bst.h
+++ bst.h
@@ -1,9 +1,7 @@
 #pragma once
-#include <cmath>
 #include <string>
 #include <vector>
 #include "transaction.h"
-
 
 struct TreeNode {
 public:
@@ -32,16 +30,11 @@ class BST {
 public:
     static TreeNode* Search(TreeNode* &p_Node, double Key);
     static TreeNode* Insert(TreeNode* &p_Node, Transaction* p_Transaction);
-    static void PrintTreeInOrder(TreeNode* p_Node);
 private:
-    static TreeNode* InsertTransaction(TreeNode* &p_Node, Transaction* p_Transaction);
-    static TreeNode* InsertNode(TreeNode* &p_Node, TreeNode* p_NewNode);
-    static TreeNode* Rebalance(TreeNode* &p_Node);
-    static TreeNode* FixImbalance(TreeNode* &p_Node);
-    static TreeNode* RotateRight(TreeNode* &p_Node);
-    static TreeNode* RotateLeft(TreeNode* &p_Node);
-    static int Height(TreeNode* &_p_Node);
-    static void ListNodesInOrder(TreeNode* &p_Node, std::vector<TreeNode*> *nodeList);
-    static void RebalanceInsert(TreeNode* &p_Node, std::vector<TreeNode*> *nodeList);
+    static TreeNode* insertTransaction(TreeNode* &p_Node, Transaction* p_Transaction);
+    static TreeNode* rebalance(TreeNode* &p_Node);
+    static TreeNode* rotateRight(TreeNode* &p_Node);
+    static TreeNode* rotateLeft(TreeNode* &p_Node);
+    static int height(TreeNode* &_p_Node);
     
 };
